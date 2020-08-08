362072032b2a9fe5298a1aae37df0bf824c33ec5
jambo <602975235@qq.com>
2020-08-08 11:44:08 +0800

    home的css和aside的html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
       <h2>基于 Vue3 的 UI 框架，开发时使用了 Vite，这个针对 Vue 单页面组件的无打包开发服务器。</h2>
       <p class="actions">
         <a href="#">GitHub</a>
-        <a href="#">Get Start</a>
+        <router-link to="/doc">Get Start</router-link>
       </p>
     </div>
   </div>
@@ -35,6 +35,27 @@
       flex-wrap: nowrap;
       > li {
         margin: 0 1em;
+      }
+    }
+  }
+  .banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+      padding: 8px 0;
+      a {
+        margin: 0 8px;
+        background: #fff;
+        display: inline-block;
+        $height: 28px;
+        height: $height;
+        line-height: $height;
+        border-radius: $height/2;
+        padding: 0 8px;
       }
     }
   }
