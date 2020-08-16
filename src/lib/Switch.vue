4c8eeba77b8bee0dada8a98b273b069beb02d0ff
jambo <602975235@qq.com>
2020-08-16 22:22:56 +0800

    switch css1

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>Switch 组件</div>
+  <button> <span></span> </button>
 </template>
 
 <script lang="ts">
@@ -8,6 +8,24 @@
   }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+  $h: 22px;
+  $h2: $h - 4px;
+  button {
+    height: $h;
+    width: $h*2;
+    border: none;
+    background: blue;
+    border-radius: $h/2;
+    position: relative;
+  }
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: #fff;
+    border-radius: $h2/2;
+  }
 </style>
