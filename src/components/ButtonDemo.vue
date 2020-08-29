6e886cb34d4c007ae4a83079c5769781a3b4d75f
jambo <602975235@qq.com>
2020-08-29 13:01:16 +0800

    生成button，并且添加默认样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,11 +2,25 @@
   <div>
     <h3>button 示例</h3>
   </div>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+  import Button from '../lib/Button.vue'
   export default {
-    name: 'ButtonDemo'
+    name: 'ButtonDemo',
+    components: {Button},
+    setup(){
+      const onClick = () => {
+        console.log('hi')
+      }
+      return { onClick }
+    }
   }
 </script>
 
