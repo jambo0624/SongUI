ca9952c3d4b792394f1d01d090e2c7f2685b5fc1
jambo <602975235@qq.com>
2020-09-06 14:10:48 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h3>dialog 示例</h3>
+    <Dialog></Dialog>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+  import Dialog from "../lib/Dialog.vue";
   export default {
-    name: 'DialogDemo'
+    name: 'DialogDemo',
+    components: {
+      Dialog
+    }
   }
 </script>
 
