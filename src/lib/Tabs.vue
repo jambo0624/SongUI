6c0371e3a64ca3173c47d732fa1e58bcd004beea
jambo <602975235@qq.com>
2020-09-21 15:10:56 +0800

    tabs nav selected

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    Tabs组件
-    <div v-for="(title, index) in titles" :key="index">{{title}}</div>
-    <component v-for="(component, index) in defaults" :key="index"
-               :is="component" />
+  <div class="song-tabs">
+    <div class="song-tabs-nav">
+      <div class="song-tabs-nav-item"
+           :class="{ selected: title === this.selected}"
+           v-for="(title, index) in titles"
+           :key="index">{{title}}</div>
+    </div>
+    <div class="song-tabs-content">
+      <component class="song-tabs-content-item"
+                 v-for="(component, index) in defaults"
+                 :key="index"
+                 :is="component" />
+    </div>
   </div>
 </template>
 
@@ -11,6 +19,11 @@
   import Tab from './Tab.vue'
   export default {
     name: "songTabs",
+    props: {
+      selected: {
+        type: String
+      }
+    },
     setup(props,context) {
       const defaults = context.slots.default()
       defaults.forEach(tag => {
@@ -26,6 +39,34 @@
   }
 </script>
 
-<style scoped>
+<style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .song-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
 
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
+          margin-left: 0;
+        }
+
+        &.selected {
+          color: $blue;
+        }
+      }
+
+    }
+
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
