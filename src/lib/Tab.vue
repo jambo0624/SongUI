1ab779af3c7eba81f84be161c76fd2978cb50c6b
jambo <602975235@qq.com>
2020-09-21 14:39:42 +0800

    先确保Tabs的子组件是Tab组件

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -4,7 +4,7 @@
 
 <script>
   export default {
-    name: "songTabs"
+    name: "songTab"
   }
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,22 @@
 <template>
-  $END$
+  <div>
+    Tabs组件
+  </div>
 </template>
 
 <script>
+  import Tab from './Tab.vue'
   export default {
-    name: "Tabs.vue"
+    name: "songTabs",
+    setup(props,context) {
+      const defaults = context.slots.default()
+      defaults.forEach(tag => {
+        if(tag.type !== Tab){
+          throw new Error('Tabs 组件的子组件必须是 Tab 组件')
+        }
+      })
+      return { defaults }
+    }
   }
 </script>
 
