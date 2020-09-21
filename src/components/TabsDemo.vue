f475aa641970c031852d6f49b258b2e6e262002c
jambo <602975235@qq.com>
2020-09-21 14:53:05 +0800

    tabs 组件里的内容用 context.slot.default() 和context.props.title渲染，Tab组件内的用 slot 直接渲染

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <h3>tabs 示例</h3>
+    <h3>tabs 示例 1</h3>
+    <Tabs>
+      <Tab title="导航 1">内容 1</Tab>
+      <Tab title="导航 2">内容 2</Tab>
+    </Tabs>
+    <h3>tabs 示例 2</h3>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+  import Tab from "../lib/Tab.vue";
+  import Tabs from "../lib/Tabs.vue";
   export default {
-    name: 'TabsDemo'
+    name: 'TabsDemo',
+    components: {
+      Tab,
+      Tabs
+    }
   }
 </script>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,13 @@
 <template>
   <div>
     Tabs组件
+    <div v-for="(title, index) in titles" :key="index">{{title}}</div>
+    <component v-for="(component, index) in defaults" :key="index"
+               :is="component" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
   import Tab from './Tab.vue'
   export default {
     name: "songTabs",
@@ -15,7 +18,10 @@
           throw new Error('Tabs 组件的子组件必须是 Tab 组件')
         }
       })
-      return { defaults }
+      const titles = defaults.map(tag => {
+        return tag.props.title
+      })
+      return { defaults, titles }
     }
   }
 </script>
