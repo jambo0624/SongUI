87ec4159b7bb135faaba67963957d363ac6e2102
jambo <602975235@qq.com>
2020-09-27 17:43:09 +0800

    添加rollup.config.js，使用rollup打包代码

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+
+import Button from "./Button.vue";
+import Dialog from "./Dialog.vue";
+import Switch from "./Switch.vue";
+import Tabs from "./Tabs.vue";
+import Tab from "./Tab.vue";
+import { openDialog } from './openDialog';
+export { Button, Dialog, Switch, Tab, Tabs, openDialog };
